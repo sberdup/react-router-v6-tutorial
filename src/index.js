ae4e1517b736a2ebf7aa5069f67870f6870e0b00
Sergio Berdeja <sergioberdeja@gmail.com>
2022-06-15 17:18:05 -0600

    added invoice id subroutes and index route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import Expenses from './routes/expenses';
 import Invoices from './routes/invoices';
+import Invoice from './routes/invoice';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -13,7 +14,25 @@ root.render(
     <Routes>
       <Route path='/' element={<App />} >
         <Route path='expenses' element={<Expenses />} />
-        <Route path='invoices' element={<Invoices />} />
+        <Route path='invoices' element={<Invoices />} >
+          {/* An index route provides a 'default' selection when no sub-route is loaded in yet */}
+          <Route index element={
+            <main style={{padding:'1rem'}}>
+              <p>Select an invoice</p>
+            </main>
+          }
+          />
+          {/* the path on this nested set of routes will define the parameter we'll grab in Invoice to map the URLs */}
+          <Route path=':invoiceId' element={<Invoice />} />
+        </Route>
+        {/* The below route handles any weird url under the default app '/' url */}
+        <Route path='*'
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
       </Route>
     </Routes>
   </BrowserRouter>

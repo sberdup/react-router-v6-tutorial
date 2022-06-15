abb72b5a5ece26d9fccd5d236fcfb9e47a81bc13
Sergio Berdeja <sergioberdeja@gmail.com>
2022-06-15 16:53:10 -0600

    imported data, added <Outlet/>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Outlet, Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <h1>Bookkeeper</h1>
+      <h1>Bookkeeper!</h1>
+      <nav style={{
+        borderBottom: 'solid 1px',
+        paddingBottom: '1rem'
+      }}
+      >
+        <Link to='/invoices'>Invoices</Link> | {' '}
+        <Link to='/expenses'>Expenses</Link>
+      </nav>
+        <Outlet/>
     </div>
   );
 }
